c00637ad2f37d8f6608ac209698b523833f4fc5e
raffinauvall <raffinauvaltaqy@gmail.com>
2025-03-21 22:48:03 +0700

    feat: add function show patient

--- Backend/clinic-api/app/Http/Controllers/PatientController.php
+++ Backend/clinic-api/app/Http/Controllers/PatientController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
 use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    //
+    public function show($id){
+        $patient = Patient::findOrFail($id);
+    }
+
+    
 }
