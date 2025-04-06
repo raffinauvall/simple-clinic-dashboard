4f99f509b9949e395a03d5ec7ff4b9075fc1ea1b
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-06 22:58:52 +0700

    chore: fixing typo at medical_record migration

--- Backend/clinic-api/database/migrations/2025_03_20_160513_create_medicalrecords_table.php
+++ Backend/clinic-api/database/migrations/2025_03_20_160513_create_medicalrecords_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('medicalrecords', function (Blueprint $table) {
+        Schema::create('medical_records', function (Blueprint $table) {
             $table->id();
             $table->foreignId('patient_id')->constrained()->onDelete('cascade');
             $table->foreignId('doctor_id')->constrained()->onDelete('cascade');
