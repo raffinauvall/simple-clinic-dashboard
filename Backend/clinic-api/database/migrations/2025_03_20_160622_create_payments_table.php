3d6be4b64deff6543672d88e0f2b8243c87b6a5a
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-06 22:49:39 +0700

    feat: add payment migration

--- Backend/clinic-api/database/migrations/2025_03_20_160622_create_payments_table.php
+++ Backend/clinic-api/database/migrations/2025_03_20_160622_create_payments_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('patient_id')->constrained()->onDelete('cascade');
+            $table->foreignId('appointment_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->string('payment_method');
+            $table->timestamp('paid_at')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
