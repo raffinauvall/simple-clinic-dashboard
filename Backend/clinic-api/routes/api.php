ed61361962c57e9b974f07fac13c542c9e383a2b
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-10 22:58:26 +0700

    feat: add routing for doctor api

--- Backend/clinic-api/routes/api.php
+++ Backend/clinic-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('patients', PatientController::class);
+
+Route::apiResource('doctors', DoctorController::class);
