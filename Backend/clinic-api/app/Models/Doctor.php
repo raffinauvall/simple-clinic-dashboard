86d0679199b8ce73635be31f1b38de24a0ac9923
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-10 22:37:15 +0700

    feat: add fillable at doctor model

--- Backend/clinic-api/app/Models/Doctor.php
+++ Backend/clinic-api/app/Models/Doctor.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'specialization',
+        'phone',
+        'email',
+        'address',
+    ];
 }
