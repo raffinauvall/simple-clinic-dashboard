079cbd3f886a61414f61aed7573dab295a227235
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-12 22:20:27 +0700

    feat: add laravel api resource and getrecordbyidpatient function

--- Backend/clinic-api/app/Http/Controllers/MedicalRecordController.php
+++ Backend/clinic-api/app/Http/Controllers/MedicalRecordController.php
@@ -2,9 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MedicalRecord;
 use Illuminate\Http\Request;
 
 class MedicalRecordController extends Controller
 {
-    //
+    public function index(){
+        $medicalrecords = MedicalRecord::all();
+
+        if($medicalrecords->isEmpty()){
+            return response()->json(['message' => 'There are no medical record yet'],404);
+        }
+        return response()->json($medicalrecords);
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+                'patient_id' => 'required|exists:patients,id',
+                'doctor_id' => 'required|exists:doctors,id',
+                'visit_date' => 'required|date',
+                'diagnosis' => 'required|string',
+                'treatment' => 'required|string',
+                'notes' => 'nullable|string',
+        ]);
+        $medicalrecords = MedicalRecord::create($validated);
+        return response()->json([
+            'message' => 'Successfully created.',
+            'data' => $medicalrecords
+    ],201);
+    }
+
+    public function show($id){
+        $medicalrecords = MedicalRecord::find($id); 
+        if (!$medicalrecords){
+            return response()->json(['message' => 'Record not found.'],404);
+        }
+        return response()->json($medicalrecords);
+    }
+
+    public function update(Request $request,$id){
+        $medicalrecords = MedicalRecord::find($id);
+        if (!$medicalrecords){
+            return response()->json(['message' => 'Record not found'],404);
+        }
+        $validated = $request->validate([
+            'visit_date' => 'sometimes|date',
+            'diagnosis' => 'sometimes|string',
+            'treatment' => 'sometimes|string',
+            'notes' => 'nullable|string',
+        ]);
+        $medicalrecords->update($validated);
+        return response()->json([
+            'message' => 'Successfully created.',
+            'data' => $medicalrecords
+        ], 201);        
+    }
+
+    public function destroy($id){
+        $medicalrecords = MedicalRecord::find($id);
+        if(!$medicalrecords){
+            return response()->json(['message' => 'Record not found'],404);
+        }
+
+        $medicalrecords->delete();
+        return response()->json(['message' => 'Successfully deleted.'],201);
+    }
+
+    public function getByPatient($patientId)
+    {
+        $medicalrecords = MedicalRecord::with('doctor', 'patient') 
+            ->where('patient_id', $patientId)
+            ->get();
+
+        if ($medicalrecords->isEmpty()) {
+            return response()->json([
+                'message' => 'No medical records found for this patient.'
+            ], 404);
+        }
+
+        return response()->json($medicalrecords);
+    }
+
 }
+
+
