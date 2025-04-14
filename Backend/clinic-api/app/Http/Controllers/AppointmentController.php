212949e70bb9a962cde6b8d04c7ed384ad9161ef
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-14 23:31:14 +0700

    feat: add index function for appointment

--- Backend/clinic-api/app/Http/Controllers/AppointmentController.php
+++ Backend/clinic-api/app/Http/Controllers/AppointmentController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
-    //
+    public function index(){
+        $appointments = Appointment::all();
+        if (!$appointments){
+            return response()->json(['message' => 'There are no appointment yet.']);
+        }
+    }
 }
