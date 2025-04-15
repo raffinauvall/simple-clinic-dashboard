55cf7130ea3f4fa889b941160a1b196e001462f1
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-15 23:27:18 +0700

    feat: add model fillable and relation

--- Backend/clinic-api/app/Models/Appointment.php
+++ Backend/clinic-api/app/Models/Appointment.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'appointment_date',
+        'appointment_time',
+        'status',
+        'notes',
+    ];
+
+    
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
