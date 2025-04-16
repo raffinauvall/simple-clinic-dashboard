4612470fd425e90f876cb7ec9224e4d93519a318
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-16 22:43:40 +0700

    feat: add model Payment

--- Backend/clinic-api/app/Models/Payment.php
+++ Backend/clinic-api/app/Models/Payment.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    protected $fillable = [
+        'patient_id',
+        'appointment_id',
+        'amount',
+        'payment_method',
+        'paid_at',
+        'notes'
+    ];
+
+    public function patients()
+    {
+        return $this->hasMany(Patient::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
     use HasFactory;
 }
