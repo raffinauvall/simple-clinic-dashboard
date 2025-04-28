ec4bb3dc9ba5f215153a569984e146c913a466d1
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-28 23:45:03 +0700

    feat: add usecase for create patient

---
+++ Frontend/clinic-dashboard/src/hooks/patient/useCreatePatient.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { createPatient } from "../../services/patientService";
+
+const useCreatePatient = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const create = async (patientData) => {
+        setLoading(true);
+        try {
+            const response = await createPatient(patientData);
+            return response;
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { create, loading, error };
+};
+
+export default useCreatePatient;
