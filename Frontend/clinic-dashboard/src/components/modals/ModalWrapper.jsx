97d55172bef6fb5a98b0b5468456fd236064a8f8
raffinauvall <raffinauvaltaqy@gmail.com>
2025-04-29 22:30:18 +0700

    chore: add modal template

---
+++ Frontend/clinic-dashboard/src/components/modals/ModalWrapper.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ModalWrapper = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white-50 bg-opacity-50">
+      <div className="bg-white dark:bg-white rounded-lg p-6 w-full max-w-lg relative">
+        <button
+          className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
+          onClick={onClose}
+        >
+          ✖️
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ModalWrapper;
