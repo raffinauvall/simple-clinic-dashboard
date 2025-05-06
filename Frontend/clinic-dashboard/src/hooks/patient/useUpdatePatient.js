baddb7ab1855d0b38356b7004051f3aa82b99e4a
raffinauvall <raffinauvaltaqy@gmail.com>
2025-05-06 23:36:26 +0700

    feat: hooks for updatePatient

--- Frontend/clinic-dashboard/src/hooks/patient/useUpdatePatient.js
+++ Frontend/clinic-dashboard/src/hooks/patient/useUpdatePatient.js
@@ -1,22 +1,24 @@
 import { useState } from "react";
-import { updatePatient } from "../../services/patientService";
+import { updatePatient } from "../../services/patientService"; // Pastikan method ini udah ada
 
 const useUpdatePatient = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const update = async (id, patientData) => {
-        setLoading(true);
-        try{
-            const response = await updatePatient(patientData);
-            return response;
-        } catch (err){ 
-            setError(err)
-        } finally{
-            setLoading(false)
-        }
+  const handleUpdate = async (id, data, onSuccess) => {
+    setLoading(true);
+    setError(null);
+    try {
+      await updatePatient(id, data);
+      if (onSuccess) onSuccess();
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
     }
-    return {loading, error, update}
-}
+  };
+
+  return { handleUpdate, loading, error };
+};
 
 export default useUpdatePatient;
