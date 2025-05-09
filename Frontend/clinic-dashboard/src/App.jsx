e92787fef669a2293d00a1d7c0b21314b942932c
raffinauvall <raffinauvaltaqy@gmail.com>
2025-05-09 23:48:02 +0700

    chore: UI for Toast sonner

--- Frontend/clinic-dashboard/src/App.jsx
+++ Frontend/clinic-dashboard/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./components/Layout";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <BrowserRouter>
+      
+      <Toaster richColors position="top-right" />
+
       <Routes>
         <Route
           path="/"
           element={
-              <Layout>
-                <Dashboard />
-              </Layout>  
+            <Layout>
+              <Dashboard />
+            </Layout>
           }
         />
       </Routes>
