ce82c354015283906af2095bc7319b1e16f63fca
raffinauvall <raffinauvaltaqy@gmail.com>
2025-06-06 22:09:30 +0700

    feat: add payment page

---
+++ Frontend/clinic-dashboard/src/pages/Payment.jsx
@@ -0,0 +1,11 @@
+import Topbar from "../components/Topbar";
+
+const Payment = () => {
+  return (
+    <div className="md:flex-row">
+      <Topbar />
+    </div>
+  );
+};
+
+export default Payment;
