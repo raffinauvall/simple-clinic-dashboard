42fdbcd624b242e9f33dd2e95cd52bdae81cf3dc
raffinauvall <raffinauvaltaqy@gmail.com>
2025-06-15 01:01:03 +0700

    feat: add patient page

--- Frontend/clinic-dashboard/src/pages/Patient.jsx
+++ Frontend/clinic-dashboard/src/pages/Patient.jsx
@@ -1,9 +1,11 @@
+import PatientBody from "../components/PatientDashboard/PatientBody";
 import Topbar from "../components/Topbar";
 
 const Patient = () => {
   return (
     <div className="md:flex-row">
       <Topbar />
+      <PatientBody />
     </div>
   );
 };
