26dc6408d663c528ccddb4075c7780625c6bc408
raffinauvall <raffinauvaltaqy@gmail.com>
2025-06-17 01:02:45 +0700

    chore: add font

--- Frontend/clinic-dashboard/src/components/PatientDashboard/PatientCard.jsx
+++ Frontend/clinic-dashboard/src/components/PatientDashboard/PatientCard.jsx
@@ -8,28 +8,28 @@ const PatientCard = ({
 }) => {
 
     return (
-         <div className="bg-white p-4 rounded-xl shadow-md flex justify-between items-start min-w-[300px]">
+         <div className="bg-white p-4 rounded-xl shadow-md flex justify-between items-start min-w-[100%]">
       <div>
-        <h3 className="text-lg font-semibold">{patient.name}</h3>
-        <p className="text-sm text-gray-600">{patient.email}</p>
-        <p className="text-sm text-gray-600">{patient.phone || "08XXXXXXXXXX"}</p>
+        <h3 className="text-2xl euclid-semibold">{patient.name}</h3>
+        <p className="text-sm text-gray-600 euclid-regular">{patient.email}</p>
+        <p className="text-sm text-gray-600 euclid-regular">{patient.phone || "08XXXXXXXXXX"}</p>
       </div>
       <div className="flex flex-col gap-2">
         <button
           onClick={() => onDetail(patient)}
-          className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700"
+          className="bg-blue-600 text-white euclid-regular px-3 py-1 rounded hover:bg-blue-700"
         >
           Detail
         </button>
         <button
           onClick={() => onEdit(patient)}
-          className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600"
+          className="bg-blue-500 text-white euclid-regular px-3 py-1 rounded hover:bg-blue-600"
         >
           Edit
         </button>
         <button
           onClick={() => onDelete(patient.id)}
-          className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+          className="bg-red-500 text-white euclid-regular px-3 py-1 rounded hover:bg-red-600"
         >
           Delete
         </button>
