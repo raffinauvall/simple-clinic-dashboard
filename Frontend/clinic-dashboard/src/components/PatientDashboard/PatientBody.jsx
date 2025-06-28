1020fd26309acc1bf7dac81881aeb5555acc6caa
raffinauvall <raffinauvaltaqy@gmail.com>
2025-06-29 01:23:06 +0700

    feat: add handler for change view

--- Frontend/clinic-dashboard/src/components/PatientDashboard/PatientBody.jsx
+++ Frontend/clinic-dashboard/src/components/PatientDashboard/PatientBody.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import PatientCard from "./PatientCard";
+import PatientTable from "./PatientTable"; // <== ini penting!
 import usePatient from "../../hooks/patient/usePatient";
 import useDeletePatient from "../../hooks/patient/useDeletePatient";
 import EditPatientModal from "../modals/PatientModal/UpdatePatientModal";
 import DeleteConfirmationModal from "../modals/DeleteConfirmationModal";
 import DetailPatientModal from "../modals/PatientModal/DetailPatientModal";
+import Navigation from "./Navigation";
 
 const PatientBody = () => {
   const {
     patients,
-    loading: isPatientLoading, 
+    loading: isPatientLoading,
     error: patientError,
     refetch,
   } = usePatient();
@@ -26,6 +28,7 @@ const PatientBody = () => {
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [selectedDeleteId, setSelectedDeleteId] = useState(null);
   const [selectedPatientId, setSelectedPatientId] = useState(null);
+  const [view, setView] = useState("grid");
 
   const handleSuccess = () => {
     refetch();
@@ -34,9 +37,9 @@ const PatientBody = () => {
   };
 
   const onDetail = (id) => {
-    setSelectedPatientId(id)
+    setSelectedPatientId(id);
     setIsDetailOpen(true);
-  }
+  };
 
   const onDelete = (id) => {
     setSelectedDeleteId(id);
@@ -57,29 +60,50 @@ const PatientBody = () => {
 
   return (
     <div className="pt-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {patients.map((patient) => (
-          <PatientCard
-            key={patient.id}
-            patient={patient}
-            onEdit={(p) => {
-              setSelectedPatient(p);
-              setIsEditOpen(true);
-            }}
-            onDelete={onDelete}
-            onDetail={() => onDetail(patient.id)}
-          />
-        ))}
-        {patients.length === 0 && (
-          <div className="text-gray-500 col-span-full">Belum ada pasien yang terdaftar.</div>
-        )}
-      </div>
+      <Navigation onChangeView={setView} />
 
-      <DetailPatientModal 
+      {view === "grid" ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {patients.map((patient) => (
+            <PatientCard
+              key={patient.id}
+              patient={patient}
+              onEdit={(p) => {
+                setSelectedPatient(p);
+                setIsEditOpen(true);
+              }}
+              onDelete={onDelete}
+              onDetail={() => onDetail(patient.id)}
+            />
+          ))}
+          {patients.length === 0 && (
+            <div className="text-gray-500 col-span-full">
+              Belum ada pasien yang terdaftar.
+            </div>
+          )}
+        </div>
+      ) : (
+        <PatientTable
+          patients={patients}
+          onEdit={(p) => {
+            setSelectedPatient(p);
+            setIsEditOpen(true);
+          }}
+          onDelete={onDelete}
+          onCreate={() => {
+            setSelectedPatient(null); // untuk create pasien baru
+            setIsEditOpen(true);
+          }}
+          isDeleteLoading={isDeleteLoading}
+          deleteError={deleteError}
+        />
+      )}
+
+      <DetailPatientModal
         isOpen={isDetailOpen}
         onClose={() => {
-          setIsDetailOpen(false)
-          setSelectedPatientId(null)
+          setIsDetailOpen(false);
+          setSelectedPatientId(null);
         }}
         patientId={selectedPatientId}
       />
