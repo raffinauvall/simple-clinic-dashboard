501869f77d91ffad1c77d42a3d9cc18baaa0f472
raffinauvall <raffinauvaltaqy@gmail.com>
2025-07-29 00:32:24 +0700

    feat: showing medical record at detail patient

--- Frontend/clinic-dashboard/src/components/modals/PatientModal/DetailPatientModal.jsx
+++ Frontend/clinic-dashboard/src/components/modals/PatientModal/DetailPatientModal.jsx
@@ -1,33 +1,59 @@
 import React from "react";
 import usePatientById from "../../../hooks/patient/usePatientbyId";
-import ModalWrapper from "../modalWrapper";
+import WrapperPatient from "./Wrapper";
 
 const DetailPatientModal = ({ isOpen, onClose, patientId }) => {
   // Jangan fetch kalau modal belum dibuka atau belum ada patientId
-  const shouldFetch = isOpen && patientId;
-  const { patient, loading, error } = usePatientById(shouldFetch ? patientId : null);
-
+  const shouldFetchPatient = isOpen && patientId;
+  const { patient, loading, error } = usePatientById(shouldFetchPatient ? patientId : null);
+  const shouldFetch = isOpen 
   return (
-    <ModalWrapper isOpen={isOpen} onClose={onClose} title="Detail Pasien">
-      {!shouldFetch ? (
+    <WrapperPatient isOpen={isOpen} onClose={onClose} title="Detail Pasien">
+      {!shouldFetchPatient ? (
         <p className="text-gray-500">Pilih pasien terlebih dahulu.</p>
       ) : loading ? (
         <p>Loading data pasien...</p>
       ) : error ? (
         <p className="text-red-500">Gagal memuat data pasien.</p>
       ) : patient ? (
-        <div className="space-y-2 text-sm">
+        <div className="flex justify-between">
+        <div className="col patient-detail space-y-2 text-sm">
+          
           <p><strong>ID:</strong> {patient.id}</p>
           <p><strong>Nama:</strong> {patient.name}</p>
           <p><strong>Gender:</strong> {patient.gender}</p>
           <p><strong>Tanggal Lahir:</strong> {patient.birth_date}</p>
           <p><strong>No. Telepon:</strong> {patient.phone}</p>
           <p><strong>Alamat:</strong> {patient.address}</p>
+         </div>
+
+       <div className="col medic-detail space-y-2 text-sm">
+        <p className="font-bold mb-1">Medical Records:</p>
+        {patient.medical_records?.length > 0 ? (
+          <ul className="list-disc ml-5 space-y-1">
+            {patient.medical_records.map((record, index) => (
+           <li key={record.id || index}>
+            <p><strong>Diagnosis:</strong> {record.diagnosis}</p>
+            <p><strong>Catatan:</strong> {record.notes}</p>
+        </li>
+      ))}
+    </ul>
+  ) : (
+   <div className="text-gray-500 space-y-2">
+      <p>Tidak ada medical record.</p>
+      <button className="bg-[#0527E6] euclid-regular text-white px-3 py-1 rounded-2xl" onClick={() => {}}
+  >
+    Tambah Record
+  </button>
+</div>
+
+  )}
+</div>
         </div>
       ) : (
         <p className="text-gray-500">Data pasien tidak ditemukan.</p>
       )}
-    </ModalWrapper>
+    </WrapperPatient>
   );
 };
 
