24a415869a96de34a6bafd2bf3966724b8e82820
raffinauvall <raffinauvaltaqy@gmail.com>
2025-08-04 23:01:26 +0700

    refactor: fixing small mistakes

--- Frontend/clinic-dashboard/src/hooks/medical-record/useCreateMedicalRecord.js
+++ Frontend/clinic-dashboard/src/hooks/medical-record/useCreateMedicalRecord.js
@@ -1,23 +1,26 @@
-import React, { use, useState } from "react";
+import { useState } from "react";
 import { createMedicalRecord } from "../../services/medicalRecordService";
 import { toast } from "sonner";
 
 const useCreateMedicalRecord = () => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false); 
+  const [error, setError] = useState(null);
 
-    const handleCreate = async (medicalRecordData) => {
-        try{
-            const response = await createMedicalRecord(medicalRecordData);
-            toast.success("Berhasil menambahkan data");
-            return response;
-        } catch (err){
-            setError(err);
-        } finally {
-            setLoading(true);
-        }
-    };
-     return {handleCreate, loading, error};
+  const handleCreate = async (medicalRecordData) => {
+    try {
+      setLoading(true);
+      const response = await createMedicalRecord(medicalRecordData);
+      toast.success("Berhasil menambahkan data");
+      return response;
+    } catch (err) {
+      setError(err);
+      toast.error("Gagal menambahkan data");
+    } finally {
+      setLoading(false); 
+    }
+  };
+
+  return { handleCreate, loading, error };
 };
 
 export default useCreateMedicalRecord;
