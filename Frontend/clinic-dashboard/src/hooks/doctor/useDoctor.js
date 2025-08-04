d37a73d32ba4fa7359a9dd580bdeb80392e910df
raffinauvall <raffinauvaltaqy@gmail.com>
2025-08-04 23:01:08 +0700

    refactor: fixing small mistakes

--- Frontend/clinic-dashboard/src/hooks/doctor/useDoctor.js
+++ Frontend/clinic-dashboard/src/hooks/doctor/useDoctor.js
@@ -1,26 +1,29 @@
+// hooks/doctor/useDoctor.js
 import { useEffect, useState } from "react";
-import { getDoctor } from "../../services/doctorService"
+import { getDoctor } from "../../services/doctorService";
 
 const useDoctor = () => {
-    const [doctor, setDoctor] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null)
+  const [doctors, setDoctors] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const fetchDoctor = async () => {
-        try {
-            setLoading(true);
-            const data = await getDoctor();
-            setDoctor(data);
-        } catch (err){
-            setError(err);
-        } finally {
-            setLoading(false);
-        }
-    };
-    useEffect(() => {
-        fetchDoctor()
-    }, []);
-    return { doctor, loading, error , refetch: fetchDoctor };
-}
+  const fetchDoctors = async () => {
+    try {
+      setLoading(true);
+      const data = await getDoctor();
+      setDoctors(data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDoctors();
+  }, []);
+
+  return { doctors, loading, error };
+};
 
 export default useDoctor;
