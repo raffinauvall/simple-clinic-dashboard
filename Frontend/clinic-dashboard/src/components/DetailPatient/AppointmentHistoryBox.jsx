c50e3bfe432b5486972a9d44231f4b41a56c1998
raffinauvall <raffinauvaltaqy@gmail.com>
2025-08-07 22:20:41 +0700

    feat: add appointment history box

---
+++ Frontend/clinic-dashboard/src/components/DetailPatient/AppointmentHistoryBox.jsx
@@ -0,0 +1,10 @@
+const AppointmentHistoryBox = ({patient}) => {
+    return (
+        <div className="p-4 rounded-xl shadow bg-white">
+            <h2 className="euclid-bold">Detail Patient</h2>
+
+        </div>
+    )
+}
+
+export default AppointmentHistoryBox;
