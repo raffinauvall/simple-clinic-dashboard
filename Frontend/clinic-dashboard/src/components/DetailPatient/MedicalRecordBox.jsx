27e64b9c30c790e70787d822c1a87f5bfbb5c071
raffinauvall <raffinauvaltaqy@gmail.com>
2025-08-07 22:20:54 +0700

    feat: add component medical record box from detail patient

---
+++ Frontend/clinic-dashboard/src/components/DetailPatient/MedicalRecordBox.jsx
@@ -0,0 +1,10 @@
+const MedicalRecordBox = ({patient}) => {
+    return (
+        <div className="p-4 rounded-xl w-1/2 shadow bg-white">
+            <h2 className="euclid-bold">Detail Patient</h2>
+             
+        </div>
+    )
+}
+
+export default MedicalRecordBox;
