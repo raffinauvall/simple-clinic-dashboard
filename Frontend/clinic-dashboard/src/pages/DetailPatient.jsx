408e3c6fd08bed8e57068c12613cca3be7179333
raffinauvall <raffinauvaltaqy@gmail.com>
2025-08-07 22:21:24 +0700

    feat: add new page for detail patient

--- Frontend/clinic-dashboard/src/pages/DetailPatient.jsx
+++ Frontend/clinic-dashboard/src/pages/DetailPatient.jsx
@@ -1,12 +1,24 @@
 import { useParams } from "react-router-dom";
 import Topbar from "../components/Topbar";
+import { useState } from "react";
+import usePatientById from "../hooks/patient/usePatientById";
+import PatientBox from "../components/DetailPatient/PatientBox"
+import MedicalRecordBox from "../components/DetailPatient/MedicalRecordBox";
+import AppointmentHistoryBox from "../components/DetailPatient/AppointmentHistoryBox";
 
 const DetailPatient = () => {
     const { id } = useParams();
+    const { patient, loading, error } = usePatientById(id);
 
     return (
         <div className="md:flex-row">
         <Topbar />
+        <div className="flex justify-between mt-3 mb-4">
+            <PatientBox patient={patient} />
+            <MedicalRecordBox/>
+           
+        </div>
+         <AppointmentHistoryBox/>
         </div>
     );
 };
